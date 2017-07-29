e0101de18f998991cf513e2052679e8f26293e12
youxiaqishi <cryouxiaqishi@sina.com>
2017-07-29 20:45:55 +0800

    clean

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+var webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+//环境变量的配置线上online 开发Dev
+var WEBPACK_ENV = process.env.WEBPACK_ENV || 'dev';
+//获取html-webpack-plugin参数的函数
+var getHtmlConfig = function(name, title){
+	return {
+		template : './src/view/'+ name +'.html',
+      	filename : 'view/'+ name +'.html',
+        title    : title,
+      	inject   : true,
+      	hash     : true,
+      	chunks   : ['common', name]
+	}
+}
+//webpack config文件
+var config = {
+    entry: {
+    	'common'  : ['./src/page/commons/index.js'],
+    	'index'   : ['./src/page/index/index.js'],
+    	'login'   : ['./src/page/login/login.js'],
+      'result'  : ['./src/page/result/index.js'],
+    },
+    output: {
+        path: "./dist",
+        publicPath : '/dist', //解决文件匹配 此时代表http://localhost:8088
+        filename: "js/[name].js"
+    },
+    externals :{
+     'jquery' : 'window.jQuery' //怎样引入jquery
+    },
+
+    module: {
+    		loaders: [
+          		{ 
+          			test: /\.css$/,
+          			loader: ExtractTextPlugin.extract("style-loader", "css-loader") //?
+          		},
+              { 
+                test: /\.string$/,
+                loader: 'html-loader'
+              },
+    		
+              {
+      　　　　　test: /\.(png|jpg|gif|woff|svg|eot|ttf)\??.*$/,
+      　　　　　loader: 'url-loader?limit=100&name=resouce/[name].[ext]' //?
+      　　　　}
+             ]
+    },
+    resolve : {
+        alias : {
+          node_modules : __dirname + '/node_modules',
+          util         : __dirname + '/src/util',
+          page         : __dirname + '/src/page',
+          server       : __dirname + '/src/server',
+          image        : __dirname + '/src/image',
+        }
+    },
+    plugins : [
+      //独立模块到js/base.js
+      new webpack.optimize.CommonsChunkPlugin({
+      	name :'common', //通用模块的处理
+      	filename : 'js/base.js'
+      }),
+      //打包css独立文件
+      new ExtractTextPlugin("css/[name].css"),
+      //html模板处理
+      new HtmlWebpackPlugin(getHtmlConfig('index','首页')),
+      new HtmlWebpackPlugin(getHtmlConfig('login', '用户登录')),
+      new HtmlWebpackPlugin(getHtmlConfig('result', '操作结果'))
+
+    ]
+};
+if('dev' === WEBPACK_ENV){
+	config.entry.common.push('webpack-dev-server/client?http://localhost:8088/');
+}
+
+
+
+module.exports  = config;
