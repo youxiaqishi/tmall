a4b70f34b9260fcb99b0b3428d69aa4f842f4fbb
youxiaqishi <cryouxiaqishi@sina.com>
2017-07-30 08:22:29 +0800

    test second

--- src/page/login/login.js
+++ src/page/login/login.js
@@ -1,2 +1,9 @@
-require('../module.js');
-console.log('hello webpack');
+/*
+* @Author: Administrator
+* @Date:   2017-07-24 21:46:04
+* @Last Modified by:   Administrator
+* @Last Modified time: 2017-07-30 08:20:29
+*/
+
+'use strict';
+var a = 12;
